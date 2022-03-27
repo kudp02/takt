0b8a12797a2acdf28f501fafac79c2e96c9e392c
Peter <peter@finefoto.ru>
2022-03-28 00:19:57 +0200

    env added

--- pages/api/file1.js
+++ pages/api/file1.js
@@ -15,7 +15,7 @@ const handler = nc({
       res.status(500).end("Something broke!");
     },
     onNoMatch: (req, res) => {
-      res.status(404).end("Page is not found");
+      res.status(404).end("Page is not found"); 
     },
   })
 
